6d06ea92639e2eb70fec5c916e854812b095629e
Kawan R. Strelow <admin@aquatechyapp.com>
2026-08-16 22:38:02 -0400

    Refactor ReactQueryProviderComponent to simplify structure and remove development tools
    - Removed the conditional rendering of ReactQueryDevtools to streamline the component.
    - Simplified the return statement for better readability and maintainability.

--- src/providers/ReactQueryProviderComponent.tsx
+++ src/providers/ReactQueryProviderComponent.tsx
@@ -1,11 +1,9 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { useState } from 'react';
 
 export function ReactQueryProviderComponent({ children }: { children: React.ReactNode }) {
-
   const [queryClient] = useState(
     () =>
       new QueryClient({
@@ -20,10 +18,5 @@ export function ReactQueryProviderComponent({ children }: { children: React.Reac
       })
   );
 
-  return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-      {process.env.NODE_ENV === 'development' && <ReactQueryDevtools />}
-    </QueryClientProvider>
-  );
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }
